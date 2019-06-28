693692e02784893289654aaedaf8c8c75e5ea768
Renan Machado <renan.mav@hotmail.com>
2019-06-28 15:19:44 -0300

    Subscribe to HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
 
 import { Hero } from "./heroes/hero";
 import { HEROES } from "./heroes/mock-heroes";
@@ -9,7 +10,7 @@ import { HEROES } from "./heroes/mock-heroes";
 export class HeroService {
   constructor() {}
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { HEROES } from "./mock-heroes";
 import { Hero } from "./hero";
+import { HeroService } from "app/hero.service";
 
 @Component({
   selector: "app-heroes",
@@ -8,15 +8,20 @@ import { Hero } from "./hero";
   styleUrls: ["./heroes.component.scss"]
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-
   selectedHero: Hero;
+  heroes: Hero[];
 
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 }
