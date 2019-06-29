7177195b1ff1d36bcf2e482c90ed3eadcdbc4c5f
Renan Machado <renan.mav@hotmail.com>
2019-06-29 13:47:30 -0300

    Refatorate HeroDetail

--- src/app/heroes/index.ts
+++ src/app/heroes/index.ts
@@ -9,17 +9,12 @@ import { HeroService } from "services/hero";
   styleUrls: ["./heroes.scss"]
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
--- src/modules/routing.ts
+++ src/modules/routing.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { HeroesComponent } from "app/heroes";
 import { DashboardComponent } from "app/dashboard";
+import { HeroDetailComponent } from 'app/hero-detail'
 
 const routes: Routes = [
   { path: "heroes", component: HeroesComponent },
   { path: "dashboard", component: DashboardComponent },
-  { path: "", redirectTo: "/dashboard", pathMatch: "full" }
+  { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+  { path: "detail/:id", component: HeroDetailComponent }
 ];
 
 @NgModule({
